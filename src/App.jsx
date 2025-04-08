133ea86865ac7cd74584b61aa93132f2ea76c030
Aung Pyae Khant <lilsnow@APKmac.local>
2025-04-08 14:46:20 +0700

    modified component changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
 import ExpenseForm from "./components/ExpenseForm"
+import ExpenseItem from "./components/ExpenseItem"
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  const handleAddExpense = (expense) => {
+    setExpenses((prevExpenses) => [expense, ...prevExpenses])
+  };
 
   return (
     <>
       <h1>Expense Tracker</h1>
-      <ExpenseForm />
+      <ExpenseForm onAddExpense={handleAddExpense} />
+      <ul>
+        {expenses.map((expense) => (
+          <li key={expense.id}>
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              category={expense.category}
+              date={expense.date} />
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
